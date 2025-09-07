5fbbf8d571931f722ba0fb2f81aac8e3fb7a8309
alongbots <aosiblings10@gmail.com>
2025-09-07 15:59:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;PIV0mBKZ#S1rY-X6HcMKmj8VaVhkmIf7W-sKAKfJ8uzJ8t7x8lrs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "639707161695", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY ALONG ( CLONE )
